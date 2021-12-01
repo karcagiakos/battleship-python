617b35a4ff3314b2255540189c2a1d3eee78ffd0
viktoriasoos <viktoria9112@gmail.com>
2021-12-01 12:09:47 +0100

    AI, shooting phase

--- battleship.py
+++ battleship.py
@@ -2,6 +2,15 @@ import time
 import os
 import string
 import copy
+import pygame
+import random
+
+pygame.init()
+pygame.font.init()
+pygame.mixer.init()
+s = "sound"
+music = pygame.mixer.music.load(os.path.join(s, "bg.flac"))
+pygame.mixer.music.play(-1)
 
 
 def clear(s):
@@ -71,8 +80,60 @@ def get_move():
     return move
 
 
-def ai_move():
+
+already_shooted = []# contain all the shots
+hit = False #False because we dont already hit a boat
+def AI_okos(move):
+    move = random.randint(0, 99)
+    if check_shot(move):
+        already_shooted.append(move)
+
+        if shooting_board[move] == 1:
+            change_shot(move)
+            hit = True
+            i = 1
+            nextshot = move + i
+
+        if shooting_board[nextshot] == 0:  #left
+            i = 1
+            nextshot = shot - 1
+            if board_player_init[nextshot] == 1:
+                change_shot(nextshot)
+                i = i + 1
+                nextshot = shot - i
+
+        elif shooting_board[nextshot] == 1:  #right
+            change_shot(nextshot)
+            i = i + 1
+            nextshot = shot + i
+
+        elif shooting_board[nextshot] == 0:
+            i = 1
+            nextshot = shot + 10
+            if shooting_board[nextshot] == 1:
+                change_shot(nextshot)
+                i = i + 10
+                nextshot = shot + i
+        elif shooting_board[nextshot] == 0: #up
+                i = 1
+                nextshot = shot - 10
+                if shooting_board[nextshot] == 1:
+                    change_shot(nextshot)
+                    i = i + 10
+                    nextshot = shot - i
     pass
+
+def AI_béla(): # just a simple IA
+    shot = random.randint(0, 99)
+    if check_shot(shot):
+        already_shooted.append(shot)
+        pass
+
+def check_shot(move):#check if the shot has not already been fired
+    for i in range(len(already_shooted)):
+        if already_shooted[i] == move:
+            return False
+        return True
 
 
 def is_valid_input(board, move):
@@ -101,9 +162,9 @@ def is_valid_input(board, move):
             return False, 0, 0
 
 
-def mark(board, row, col):
-    if board[row][col] == "🇴":
-        board[row][col] = "🇭"
+# def mark(board, row, col):
+#     if board[row][col] == "🇴":
+#         board[row][col] = "🇭"
 
 
 def win_conditions():
@@ -122,9 +183,30 @@ def placement_phase(board):
     clear(0)
 
 
-def shooting_phase():
-    pass
+def check_shot(shot):#check if the shot has not already been fired
+    for i in range(len(already_shooted)):
+        if already_shooted[i] == shot:
+            return False
+        return True
 
+def shooting_phase(player_1,player_1_placement_board):
+    board = player_1_placement_board 
+    player = player_1 #change_player(player)
+    move = get_move()
+    # board = [player_1_placement_board, player_2_placement_board] #change_board():
+    is_valid, row, col = is_valid_input(board, move)
+    if  board[row][col] != "🇽":
+        print("Missed!")
+        change_shot(move)
+        print(board)
+    elif board[row][col] == "🇽":
+        pass
+
+
+def change_shot(move):
+    del board_player_init[move]
+    board_player_init.insert(move, "🇽")
+    board[row][col] = "🇲"
 
 def main():
     clear(0)
@@ -132,13 +214,14 @@ def main():
     clear(1)
     size = board_size()
     player_1_placement_board = init_board(size)
+    #player_1_placement_board[1][1] = '🇽'
     # player_2_placement_board = copy.deepcopy(player_1_placement_board)
     # player_1_shooting_board = copy.deepcopy(player_1_placement_board)
     # player_2_shooting_board = copy.deepcopy(player_1_placement_board)
     clear(1)
     while True:
         placement_phase(player_1_placement_board)
-        shooting_phase()
+        shooting_phase(player1,player_1_placement_board)
         if win_conditions():
             return
 
